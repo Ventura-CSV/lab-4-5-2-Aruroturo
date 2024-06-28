2c28cee96211e9648f15165def60212521165893
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-27 21:21:04 -0700

    again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-
 def main():
     total = 0
     numbers = []
@@ -8,6 +7,10 @@ def main():
         random_num = random. randint(1,100)
         numbers.append(random_num)
         total += random_num
+    sum_less_tan_100 = 0
+    for num in numbers:
+        if num< 100:
+            sum_less_tan_100 += 100
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
